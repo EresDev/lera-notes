e42d985cd1e6e057d017457c0ee2ca1d57e9d900
EresDev <hi@eresdev.com>
2021-02-03 19:00:06 +0500

    feat: append text to the file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js')
+//fs.writeFileSync('notes.txt', 'This file was created by Node.js')
+fs.appendFileSync('notes.txt', '\nAppend the message')
