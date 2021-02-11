544ee739d4c874da6addadf8e3aa4957100b8d5b
EresDev <hi@eresdev.com>
2021-02-11 20:45:07 +0500

    feat: save note to file

--- notes.js
+++ notes.js
@@ -1,4 +1,46 @@
+const fs = require('fs')
+
 const getNotes = function (){
     return "Your notes..."
 }
-module.exports = getNotes
+
+const addNote = function addNote(title, body) {
+    const notes = loadNotes()
+
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title
+    })
+
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        })
+    
+        saveNotes(notes)
+        console.log("Note saved.")
+    } else {
+        console.log("Duplicate note is not saved.")
+    }
+}
+
+const saveNotes = function(notes){
+    const notesStr = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', notesStr)
+}
+
+const loadNotes = function () {
+    try {
+        const buffer = fs.readFileSync('notes.json')
+        const notesStr = buffer.toString()
+        const notesJSON = JSON.parse(notesStr)
+        return notesJSON
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
